ca204e3d25489085191a6e00f242b8e38b444ca7
Ovidiu Miron <ovidiu.miron@ammeon.com>
2017-06-06 16:26:42 +0100

    scan

--- demo3/scan.py
+++ demo3/scan.py
@@ -5,7 +5,9 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('math')
     response = table.scan(
-    FilterExpression=Attr('result').exists()
+    FilterExpression=Attr('result').non_exists()
     )
+    # see
+    # http://boto3.readthedocs.io/en/latest/_modules/boto3/dynamodb/conditions.html
     items = response['Items']
     return items
